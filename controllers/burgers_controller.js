1511405c06c7daa001f952cc6cae8d61054acf75
Laura Aupert <aupert.laura@gmail.com>
2021-02-13 14:03:54 -0500

    With Burger devour now button through handlebars partial

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,4 +14,37 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.put('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log('condition', condition);
+  
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      condition,
+      (result) => {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
+  
+  router.delete('/api/burgers/:id', (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    burger.delete(condition, (result) => {
+      if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    });
+  });
+  
+
 module.exports = router
