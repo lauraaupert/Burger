ad6c2f16014a08c31c8d6b7a8d9078257bce7b04
Laura Aupert <aupert.laura@gmail.com>
2021-02-14 15:46:26 -0500

    Corrected the code for database connection with JawsDB

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,14 @@ const mysql = require('mysql');
 var connection;
 
 if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection({
-    // process.env.JAWSDB_URL
-    host: '	qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    port: 3306,
-    user: 'hm0a9cqfjkpro2ne',
-    password: 'lxrvzjohyv8tz5s7',
-    database: 'coa0v38yiwowob86',
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  //   host: '	qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  //   port: 3306,
+  //   user: 'hm0a9cqfjkpro2ne',
+  //   password: 'lxrvzjohyv8tz5s7',
+  //   database: 'coa0v38yiwowob86',
   
-  })
+  // })
 } else {connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
